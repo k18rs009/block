dd6a5adfc4f272bb63ab4268c3f56ad7f1c3176d
k18rs009 <k18rs009@st.kyusan-u.ac.jp>
2019-12-02 14:28:53 +0900

    順位

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -1,4 +1,55 @@
 // This is a JavaScript file
 var ncmbController = {
+APPLICATION_KEY: "16163651a83a330255700f837a3e714a600793df51aefc9b490b432cd596c36c",
+    CLIENT_KEY: "dbae0be801b216871919838ca022d40011928411e4a88ed2b5ba62eebee2f71f",
+    sendScore: function(score) {
+    var self = this;
 
+    // [1]Score（クラス）を生成
+    var Score = self.ncmb.DataStore("ScoreClass");
+
+    // [2]インスタンス生成、スコア数値をフィールド名"score"にセット
+    var scoreData = new Score({score: score});
+
+    // [3]送信処理
+    scoreData.save()
+        .then(function (saved) {
+            Score.greaterThan("score", score)
+    .count()    // 件数を結果に含める
+    .fetchAll()
+    .then(function(scores){
+        // countの結果は、取得データscoresのcountプロパティに含まれる
+
+        // 0件のとき正しく動作するように条件分岐
+        var rank = (scores.count !== undefined) ? parseInt(scores.count) + 1 : 1;
+
+        // ダイアログの表示
+        if(typeof navigator.notification !== 'undefined'){
+            navigator.notification.alert(
+                "今回の順位は #" + rank + " でした！",
+                function(){},
+                "スコア送信完了！"
+                );
+        } else {
+            alert("スコア送信完了！\n今回の順位は #" + rank + " でした！");
+        }
+    })
+        })
+       .catch(function(err){
+            console.log(err);
+        });
+},
+
+    ncmb: null,
+    currentUser: null,  // ログインしたユーザーのオブジェクトを格納
+    screenSize: null,    // 画面サイズを格納
+
+    // 初期化
+    init: function(screenSize) {
+        var self = this;
+        self.ncmb = new NCMB(self.APPLICATION_KEY, self.CLIENT_KEY);    // mobile backendの初期化
+        self.screenSize = screenSize;
+        
+    }
+    
 }
